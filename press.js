6a9b827f9671d3985ccf5709b765bbe590a605c3
Timofei Utkin <timofei.utkin@integrate.io>
2022-06-20 16:57:52 +0900

    can try to press 3 times before exiting without an error

--- press.js
+++ press.js
@@ -1,18 +1,29 @@
-// Load the node-switchbot and get a `Switchbot` constructor object
+// Load the node-switchbot
 const Switchbot = require('node-switchbot');
-// Create an `Switchbot` object
-const switchbot = new Switchbot();
 
 (async () => {
-  // Find a Bot (WoHand)
-  const bot_list = await switchbot.discover({ model: 'H'});
-  if (bot_list.length === 0) {
-    throw new Error('No device was found.');
+  // Create an `Switchbot` object
+  const switchbot = new Switchbot();
+  let numberOfTries = 0;
+  let success = false;
+  while (numberOfTries < 3 && !success) {
+    //Find a Bot (WoHand)
+    const bot_list = await switchbot.discover({ model: 'H'});
+    
+    if (bot_list.length < 1) {
+      console.log('No devices were found. Searching again ' + (3 - numberOfTries) + ' more time(s).')
+      numberOfTries++;
+    } else {
+      // The `SwitchbotDeviceWoHand` object representing the found Bot.
+      let device = bot_list[0];
+      // Press the Bot's arm
+      await device.press();
+      success = true;
+      console.log('Successfully pressed :)');
+    }
   }
-  // The `SwitchbotDeviceWoHand` object representing the found Bot.
-  let device = bot_list[0];
-  // Press the Bot's arm
-  await device.press();
-
+  if (!success) {
+    console.log('Aborting.')
+  }
   process.exit();
 })();
